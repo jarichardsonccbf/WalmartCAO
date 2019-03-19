0cd0043e2ee263e3a429e3f6e99e07c2448f94d8
jarichardsonccbf <jarichardson@cocacolaFlorida.com>
2019-03-19 13:06:36 -0400

    process has been automated

--- WalmartCAO.R
+++ WalmartCAO.R
@@ -12,66 +12,36 @@ itmnmbr <- read.csv("itmnmbr.upc.csv", stringsAsFactors = FALSE)
 CAO.units.cases <- CAO.units.cases %>% 
   mutate(Units.per.case = Units.Ordered..SUM./Cases.Ordered..SUM.)
 
-#percentage of perfect matches----
+#df subsets
 
-#number of perfect matches
-perfect_match <- CAOwk7 %>% 
-  filter(Case.Difference == 0) %>% 
-  count(n()) %>% 
-  select(n)
+#just CCBF
+CAOwk7.CCBF <- CAOwk7 %>% 
+  filter(OWNER == "CCBF")
 
-#Get %age of perfect matches
-perfect_match / nrow(CAOwk7) * 100
-#19.56291% of orders match
-
-#Does %age change if we remove instances of WUS, WUoH, and WAMOU at 0
-zeroes.out <- CAOwk7 %>% 
+#just in stocks
+CAOwk7.instocks <- CAOwk7 %>% 
   filter(Weekly.Unit.Sales != 0 & Weekly.Units.On.Hand != 0 & Weekly.AM.Order.Units > 0)
 
-perfect_match_sub <- zeroes.out %>% 
-  filter(Case.Difference == 0) %>% 
-  count(n()) %>% 
-  select(n)
-
-perfect_match_sub / nrow(zeroes.out) * 100
-#8.702771% of orders match
-
-#Do values differ for our stores?
-
-#number of perfect matches
-perfect_match_CF <- CAOwk7 %>% 
-  filter(OWNER == "CCBF") %>% 
-  filter(Case.Difference == 0) %>% 
-  count(n()) %>% 
-  select(n)
-
-#Get %age of perfect matches
-perfect_match_CF / nrow(CAOwk7 %>% 
-                          filter(OWNER == "CCBF")) * 100
-#18.540% for us vs 19.56291% whole
-
-#Does %age change if we remove instances of WUS, WUoH, and WAMOU at 0
-zeroes.out.CF <- CAOwk7 %>%
+#ccbf instocks
+CAOwk7.ccbf.instocks <- CAOwk7 %>% 
   filter(OWNER == "CCBF") %>% 
   filter(Weekly.Unit.Sales != 0 & Weekly.Units.On.Hand != 0 & Weekly.AM.Order.Units > 0)
 
-perfect_match_sub.CF <- zeroes.out.CF %>% 
-  filter(Case.Difference == 0) %>% 
-  count(n()) %>% 
-  select(n)
-
-perfect_match_sub.CF / nrow(zeroes.out.CF) * 100
-#6.66% vs 8.702771% for whole
-
-#RMSE----
-RMSE = function(actual, predicted){
-  sqrt(mean((actual - predicted)^2))
+#Function to calculate interesting metrics for each
+CAO <- function(df) {
+  ppm = df %>% 
+    filter(Case.Difference == 0) %>% 
+    count(n()) %>% 
+    select(n) / nrow(df) * 100
+  
+  rms = sqrt(mean((df$Weekly.AM.Order.Units - df$Weekly.GRS.Order.Units)^2))
+  
+  mae = mean(abs(df$Case.Difference))
+  
+  list(Percentage.Perfect = ppm, RMSE = rms, MAE = mae)
 }
 
-RMSE(CAOwk7$Weekly.AM.Order.Units, CAOwk7$Weekly.GRS.Order.Units)
-#37.51782
-
-#Mean absolute difference----
-mean(abs(CAOwk7$Case.Difference))
-hist(abs(CAOwk7$Case.Difference), breaks = 400)
-
+CAO(CAOwk7)
+CAO(CAOwk7.CCBF)
+CAO(CAOwk7.instocks)
+CAO(CAOwk7.ccbf.instocks)
